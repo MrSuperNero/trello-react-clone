4e9f9b0abad81a22e674375541efa77d86833c26
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-08-05 11:31:50 -0400

    removed gitignore

---
+++ build/precache-manifest.5cadcde305a7534fcdec59097aff213b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7750639a70c9cc7533083746079510b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "629e4472f641f8378512",
+    "url": "/static/css/main.aae39f5e.chunk.css"
+  },
+  {
+    "revision": "d43daa28a8e7342c2e6a",
+    "url": "/static/js/2.177e171a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.177e171a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629e4472f641f8378512",
+    "url": "/static/js/main.e6b4de50.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  },
+  {
+    "revision": "887c7428e80e32cd849875f48bcf6596",
+    "url": "/static/media/background.887c7428.jpg"
+  }
+]);
